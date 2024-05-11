380453163b48d0457512c82af0da5e4ff3e545cb
Kyb3r Cipher <me@kyb3rcipher.com>
2024-05-11 15:00:57 -0600

    Make responsive mobile navbar

--- js/home.js
+++ js/home.js
@@ -1,2 +1,30 @@
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
+
+
+// Disable scroll in mobile responsive navbar
+document.addEventListener('DOMContentLoaded', function() {
+    var checkBtn = document.getElementById('check');
+    var body = document.querySelector('body');
+
+    checkBtn.onclick = function() {
+        body.classList.toggle('no-scroll');
+        if (this.checked) {
+            disableScroll();
+        } else {
+            enableScroll();
+        }
+    };
+
+    function disableScroll() {
+        document.addEventListener('touchmove', preventDefault, { passive: false });
+    }
+    function enableScroll() {
+        document.removeEventListener('touchmove', preventDefault, { passive: false });
+    }
+
+    // Función para prevenir el comportamiento predeterminado del desplazamiento táctil
+    function preventDefault(e) {
+        e.preventDefault();
+    }
+});
