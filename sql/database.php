2137fc2af8a172ce6a1e2b935fca1a8769acbd82
Kyb3r Cipher <me@kyb3rcipher.com>
2024-06-05 22:11:53 -0600

    Create correctly database

--- sql/database.php
+++ sql/database.php
@@ -3,12 +3,12 @@ function connectToDatabase() {
     $host = "sql200.infinityfree.com";
     $username = "if0_36647081";
     $password = "nmT0QyUZksK";
-    $database = "if0_36647081_database";
+    $database = "if0_36647081_galispace";
 
     if (isset($host, $username, $password)) {
         $connection = new mysqli($host, $username, $password, $database);
     } else {
-        $connection = new mysqli("localhost", "root", "", "database");
+        $connection = new mysqli("localhost", "root", "", "galispace");
     }
 
     if ($connection->connect_error) {
