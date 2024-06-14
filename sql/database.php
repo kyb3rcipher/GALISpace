427a39ec85a76eb019f9f8a1add41f23f1d9915a
Kyb3r Cipher <me@kyb3rcipher.com>
2024-06-13 22:13:55 -0600

    Back to local database

--- sql/database.php
+++ sql/database.php
@@ -1,15 +1,6 @@
 <?php
 function connectToDatabase() {
-    $host = "sql200.infinityfree.com";
-    $username = "if0_36647081";
-    $password = "nmT0QyUZksK";
-    $database = "if0_36647081_galispace";
-
-    if (isset($host, $username, $password)) {
-        $connection = new mysqli($host, $username, $password, $database);
-    } else {
-        $connection = new mysqli("localhost", "root", "", "galispace");
-    }
+    $connection = new mysqli("localhost", "root", "", "galispace");
 
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
